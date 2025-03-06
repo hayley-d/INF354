35072dfcd93fdf0343758284c87b096b0f1664d3
Hayley Dodkins <hayleydod@proton.me>
2025-03-06 17:12:22 +0200

    help me

--- Assignment_1/u21528790_HW01_API/u21528790_HW01_API.Tests/Controller/ProductControllerTest.cs
+++ Assignment_1/u21528790_HW01_API/u21528790_HW01_API.Tests/Controller/ProductControllerTest.cs
@@ -20,7 +20,7 @@ namespace u21528790_HW01_API.Tests
             var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(databaseName: "TestDb").Options;
             var dbContext = new AppDbContext(options);
             await dbContext.Database.EnsureCreatedAsync();
-            dbContext.Products.Add(new Product { ID = 1, Name = "Test Prod", Description = "Desc", Price = 100 });
+            dbContext.Products.Add(new Product { ID = 2, Name = "Test Prod", Description = "Desc", Price = 100 });
             await dbContext.SaveChangesAsync();
             return dbContext;
         }
@@ -31,11 +31,38 @@ namespace u21528790_HW01_API.Tests
             var dbContext = await GetDbContext();
             var controller = new ProductController(dbContext);
 
-            var res = await controller.GetProduct(1);
+            var res = await controller.GetProduct(2);
 
             var okRes = Assert.IsType<OkObjectResult>(res);
             var product = Assert.IsType<Product>(okRes.Value);
-            Assert.Equal(1, product.ID);
+            Assert.Equal(2, product.ID);
+            
+        }
+
+        public async Task GetProducts_Returns_List()
+        {
+            var dbContext = await GetDbContext();
+            var controller = new ProductController(dbContext);
+
+            var res = await controller.GetProducts();
+
+            var okRes = Assert.IsType<OkObjectResult>(res);
+            int products = Assert.IsType<IEnumerable<Product>>(okRes.Value).Count();
+            Assert.Equal(products, 2);
+            var res_2 = controller.DeleteProduct(2);
+            var okR = Assert.IsType<OkObjectResult>(res_2);
+        }
+
+
+        [Fact]
+        public async Task GetProduct_Returns_NotFound_When_Not_Exists()
+        {
+            var dbContext = await GetDbContext();
+            var controller = new ProductController(dbContext);
+
+            var res = await controller.GetProduct(9999);
+
+            var okRes = Assert.IsType<NotFoundResult>(res);
         }
     }
 }
--- Assignment_1/u21528790_HW01_API/u21528790_HW01_API.Tests/obj/Debug/net8.0/u21528790_HW01_API.Tests.AssemblyInfo.cs
+++ Assignment_1/u21528790_HW01_API/u21528790_HW01_API.Tests/obj/Debug/net8.0/u21528790_HW01_API.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("u21528790_HW01_API.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d0c942753e9d2c05a8c69ceb803326274efb84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9490e06938154488ac8af4022ce72656545d826")]
 [assembly: System.Reflection.AssemblyProductAttribute("u21528790_HW01_API.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("u21528790_HW01_API.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
