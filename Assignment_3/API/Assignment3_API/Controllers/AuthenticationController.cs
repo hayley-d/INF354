ae015afa01f5492b5d20923dac36127c85acac3e
Hayley Dodkins <hayleydod@proton.me>
2025-05-09 01:54:51 +0200

    Add basic function for login controller

--- Assignment_3/API/Assignment3_API/Controllers/AuthenticationController.cs
+++ Assignment_3/API/Assignment3_API/Controllers/AuthenticationController.cs
@@ -4,7 +4,20 @@ namespace Assignment3_API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class AuthenticationController
+    public class AuthenticationController : ControllerBase
     {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDto loginDto)
+        {
+            // TODO: Validate cradentials
+            return Ok(/*Auth Token here*/);
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterDto registerDto) { 
+            // TODO: Create new user
+            return Ok(/* Confirmation message*/);
+        }
+
     }
 }
